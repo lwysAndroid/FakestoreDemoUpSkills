f67a1b82d2c2af10fddefb11e21459280395d6cd
LuisTovar <lwys.tovar.war@gmail.com>
2023-03-23 22:26:48 -0600

    Save tokens in share preferences

--- app/src/main/java/com/chemasmas/fakestoreapi/core/data/repository/implementations/TokensRepositoryImp.kt
+++ app/src/main/java/com/chemasmas/fakestoreapi/core/data/repository/implementations/TokensRepositoryImp.kt
@@ -1,32 +1,51 @@
 package com.chemasmas.fakestoreapi.core.data.repository.implementations
 
+import android.content.Context
+import android.content.SharedPreferences
+import com.chemasmas.fakestoreapi.R
 import com.chemasmas.fakestoreapi.core.data.repository.TokensRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-class TokensRepositoryImp @Inject constructor() : TokensRepository {
+class TokensRepositoryImp @Inject constructor(
+    @ApplicationContext private val context: Context
+) : TokensRepository {
 
-    companion object {
-        private var refresh = ""
-        private var access = ""
+    private val sharePreferences: SharedPreferences by lazy { getSharePReferences() }
+    private fun getSharePReferences(): SharedPreferences {
+        return context.getSharedPreferences(
+            context.getString(R.string.preference_tokens_key), Context.MODE_PRIVATE
+        )
     }
 
     override suspend fun setRefreshToken(refresh: String) {
-        TokensRepositoryImp.refresh = refresh
+        with(sharePreferences.edit()) {
+            putString(context.getString(R.string.refresh_token), refresh)
+            apply()
+        }
     }
 
     override suspend fun getRefreshToken(): String {
-        return refresh
+        return with(sharePreferences) {
+            getString(context.getString(R.string.refresh_token), "") ?: ""
+        }
     }
 
     override suspend fun setAccessToken(access: String) {
-        TokensRepositoryImp.access = access
+        with(sharePreferences.edit()) {
+            putString(context.getString(R.string.access_token), access)
+            apply()
+        }
     }
 
     override suspend fun getAccessToken(): String {
-        return access
+        return with(sharePreferences) {
+            getString(context.getString(R.string.access_token), "") ?: ""
+        }
     }
 
     override suspend fun getBearerAccessToken(): String {
-        return "Bearer $access"
+        return "Bearer ${getAccessToken()}"
     }
+
 }
